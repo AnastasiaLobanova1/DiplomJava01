9700a269a3cbe8df10bba0015e9adb2a4507e7ae
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-07-12 13:53:43 +0500

    tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -8,14 +8,118 @@ public class SavingAccountTest {
     @Test
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
+                2000,
+                1000,
+                10000,
                 5
         );
+        account.add(3000);
+        Assertions.assertEquals(2000 + 3000, account.getBalance());
+    }
 
-        account.add(3_000);
+    @Test
+    public void shouldAddOverThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        account.add(13000);
+        Assertions.assertEquals(2000, account.getBalance());
+    }
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    @Test
+    public void shouldAddBeforeNull() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        account.add(-2000);
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayOverThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        account.pay(500);
+        Assertions.assertEquals(2000 - 500, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayLessThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        account.pay(5000);
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayBeforeNull() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        account.pay(-2000);
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+    @Test
+    public void testYearChange() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        int actual = account.yearChange();
+        Assertions.assertEquals( 2000 / 100 * 5, actual);
+    }
+
+    @Test
+    public void testRateBeforeNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            new SavingAccount(
+                    2000,
+                    1000,
+                    10000,
+                    -10
+            );
+        });
+    }
+
+    @Test
+    public void testGetMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        Assertions.assertEquals(1000, account.getMinBalance());
+    }
+
+    @Test
+    public void testGetMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                1000,
+                10000,
+                5
+        );
+        Assertions.assertEquals(10000, account.getMaxBalance());
     }
 }
